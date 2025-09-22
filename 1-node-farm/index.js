eb03d0c5111263ed3223702172f48c2ac41e3913
talhaceliktas <avrol928@gmail.com>
2025-09-22 22:22:41 +0300

    added eslint

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,10 +1,40 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", { encoding: "utf-8" });
-console.log(textIn);
+////// SYNC WAY
+function syncWay() {
+  const textIn = fs.readFileSync("./txt/input.txt", { encoding: "utf-8" });
+  console.log(textIn);
 
-const textOut = `This is what we know about the avacado ${textIn}.\nCreated on ${new Date(
-  Date.now()
-).toLocaleString()}`;
-fs.writeFileSync("./txt/new-file.txt", textOut);
-console.log("File written!");
+  const textOut = `This is what we know about the avacado ${textIn}.\nCreated on ${new Date(
+    Date.now()
+  ).toLocaleString()}`;
+  fs.writeFileSync("./txt/new-file.txt", textOut);
+  console.log("File written!");
+}
+
+////// ASYNC WAY
+
+// callback hell
+function callbackHell() {
+  fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+      console.log(data2);
+      fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+        console.log(data3);
+      });
+    });
+  });
+}
+
+const fsPromises = require("fs/promises");
+// clean async
+async function cleanAsync() {
+  const startData = await fsPromises.readFile("./txt/start.txt", "utf-8");
+  const readThisData = await fsPromises.readFile(
+    `./txt/${startData}.txt`,
+    "utf-8"
+  );
+  console.log(readThisData);
+}
+
+callbackHell();
