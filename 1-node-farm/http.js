00f9eeb682cd88d36326625bd54485c8792c6da0
talhaceliktas <avrol928@gmail.com>
2025-09-23 20:58:46 +0300

    add routing

--- 1-node-farm/http.js
+++ 1-node-farm/http.js
@@ -1,7 +1,19 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview")
+    res.end("THIS IS THE OVERVIEW");
+  else if (pathName === "/product") res.end("THIS IS THE PRODUCT");
+  else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("Page not found!");
+  }
 });
 
 server.listen(8000, () => {
