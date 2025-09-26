b0a12c4fbff776c9238728c1896bce56acfbb791
talhaceliktas <avrol928@gmail.com>
2025-09-26 15:22:06 +0300

    add eventemitter

---
+++ 2-how-node-works/event.js
@@ -0,0 +1,17 @@
+const EventEmitter = require("events");
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on("newSale", () => {
+  console.log("There was a new sale");
+});
+
+myEmitter.on("newSale", () => {
+  console.log("Customer Name: Talha");
+});
+
+myEmitter.on("newSale", (stock) => {
+  console.log(`There are now ${stock} items left in stock`);
+});
+
+myEmitter.emit("newSale", 9);
