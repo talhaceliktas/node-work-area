db01e1b9aac02b4831abae88c5f78079223e5508
talhaceliktas <avrol928@gmail.com>
2025-09-26 15:46:10 +0300

    add small api for listening

--- 2-how-node-works/event.js
+++ 2-how-node-works/event.js
@@ -1,6 +1,13 @@
 const EventEmitter = require("events");
+const http = require("http");
 
-const myEmitter = new EventEmitter();
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sales();
 
 myEmitter.on("newSale", () => {
   console.log("There was a new sale");
@@ -15,3 +22,24 @@ myEmitter.on("newSale", (stock) => {
 });
 
 myEmitter.emit("newSale", 9);
+
+///////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request received");
+  res.end("Request received");
+});
+
+server.on("request", (req, res) => {
+  console.log("Another request");
+});
+
+server.on("close", () => {
+  console.log("Server closed");
+});
+
+server.listen(8000, () => {
+  console.log("Waiting for request");
+});
