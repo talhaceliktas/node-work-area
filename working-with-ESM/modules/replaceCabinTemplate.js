fca2237a78be8d6d60e369dd513461f918e2b365
talhaceliktas <avrol928@gmail.com>
2025-09-27 14:52:05 +0300

    seperate hotel project other repo

--- working-with-ESM/modules/replaceCabinTemplate.js
+++
@@ -1,8 +0,0 @@
-const replaceCabinTemplate = (templateCabin, data) => {
-  return templateCabin
-    .replace(/{%NAME%}/g, data.name)
-    .replace(/{%MAXCAPACITY%}/g, data.maxCapacity)
-    .replace(/{%PRICE%}/g, data.regularPrice - data.discount);
-};
-
-export default replaceCabinTemplate;
